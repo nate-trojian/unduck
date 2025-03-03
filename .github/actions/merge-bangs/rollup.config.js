797816da068cdbedc3388729d8b61dafcd5d6ca7
Nate Trojian <ntrojian@gmail.com>
2025-03-03 14:16:20 -0500

    Add rollup plugin to import json

--- .github/actions/merge-bangs/rollup.config.js
+++ .github/actions/merge-bangs/rollup.config.js
@@ -1,5 +1,6 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
 
 const config = {
   input: "index.js",
@@ -9,7 +10,7 @@ const config = {
     format: "es",
     sourcemap: true,
   },
-  plugins: [commonjs(), nodeResolve({ preferBuiltins: true })],
+  plugins: [commonjs(), nodeResolve({ preferBuiltins: true }), json()],
 };
 
 export default config;
