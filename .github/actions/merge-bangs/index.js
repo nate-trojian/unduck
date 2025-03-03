197cab2bc5e73104c6aa3b1c57287f9333e1ca77
Nate Trojian <ntrojian@gmail.com>
2025-03-03 14:18:16 -0500

    Imports are static, not async

--- .github/actions/merge-bangs/index.js
+++ .github/actions/merge-bangs/index.js
@@ -4,10 +4,8 @@ import ddgBangsFile from "../../../latest-ddg-bangs.json" assert { type: "json" 
 import overwritesFile from "../../../overwrites.json" assert { type: "json" };
 
 try {
-  let ddgBangs = ddgBangsFile.then((bangs) => bangs.map((b) => [b.t, b]));
-  let overwrites = overwritesFile.then((overwrites) =>
-    overwrites.map((b) => [b.t, b])
-  );
+  let ddgBangs = ddgBangsFile.map((b) => [b.t, b]);
+  let overwrites = overwritesFile.map((b) => [b.t, b]);
   let final = new Map([...ddgBangs, ...overwrites]);
   core.info(JSON.stringify(final));
 } catch (e) {
